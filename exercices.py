adff46acb4423b29b00d5c20bdb94ae846297e85
Brian Ruiz <braymr33@gmail.com>
2023-06-01 23:11:00 -0600

    2 code challenges

---
+++ exercices.py
@@ -0,0 +1,31 @@
+"""
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+name + " plays banjo" 
+name + " does not play banjo"
+"""
+
+
+def are_you_playing_banjo(name):
+    # Your code here
+    pass
+
+
+"""
+There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+
+You receive an array with your peers' test scores. Now calculate the average and compare your score!
+
+Return True if you're better, else False!
+
+Note:
+Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
+"""
+
+
+def better_than_average(class_points, your_points):
+    # Your code here
+    pass
